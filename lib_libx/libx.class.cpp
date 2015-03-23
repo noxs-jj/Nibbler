31e566aa0bd2eaa2fad257783adf3d88d6f77769
Jean-Jacques MOIROUX <jmoiroux@student.42.fr>
2015-03-23 16:20:41 +0100

    update

--- lib_libx/libx.class.cpp
+++ lib_libx/libx.class.cpp
@@ -9,3 +9,21 @@
 //   Updated: 2015/03/16 15:40:58 by jmoiroux         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
+
+#include "libx.class.hpp"
+
+lib_libx( lib_libx const &bsrc ) {
+
+}
+
+lib_libx & operation=(lib_libx const &brhs) {
+
+}
+
+lib_libx( void ) {
+
+}
+
+~lib_libx() {
+
+}
--- lib_libx/libx.class.hpp
+++ lib_libx/libx.class.hpp
@@ -8,9 +8,14 @@ typedef struct		s_key
 	struct s_key	*next;
 }					t_key;
 
-class lib_libx : public api
-{
-	public:
-		lib_libx();
-		~lib_libx();
+class lib_libx : public api {
+public:
+	lib_libx( void );
+	lib_libx( lib_libx const &bsrc );
+	lib_libx & operation=(lib_libx const &brhs);
+	~lib_libx();
+
+private:
+	
+
 };
