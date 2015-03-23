7a742ac39d2f692ea61f6d9a0d8cfbc8f8bd4607
Jean-Jacques MOIROUX <jmoiroux@student.42.fr>
2015-03-23 16:14:09 +0100

    struct key

--- includes/main.hpp
+++ includes/main.hpp
@@ -9,3 +9,14 @@
 //   Updated: 2015/03/16 15:36:15 by jmoiroux         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
+
+#ifndef MAIN_HPP
+# define MAIN_HPP
+
+typedef struct		s_key
+{
+	int				key;
+	struct s_key	*next;
+}					t_key;
+
+#endif
--- lib_libx/libx.class.hpp
+++ lib_libx/libx.class.hpp
@@ -2,6 +2,12 @@
 
 class api;
 
+typedef struct		s_key
+{
+	int				key;
+	struct s_key	*next;
+}					t_key;
+
 class lib_libx : public api
 {
 	public:
