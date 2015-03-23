b2abd3b7b0281d34b0cb8e758c87f1d5285d53a8
Jean-Jacques MOIROUX <jmoiroux@student.42.fr>
2015-03-23 18:06:49 +0100

    update libx

--- lib_libx/libx.class.cpp
+++ lib_libx/libx.class.cpp
@@ -12,36 +12,48 @@
 
 #include "libx.class.hpp"
 
-lib_libx::lib_libx( lib_libx const &bsrc ) {
-
+lib_libx::lib_libx( lib_libx const & src ) {
+	*this = src;
 }
 
-lib_libx::lib_libx & operation=(lib_libx const &brhs) {
-
+lib_libx & lib_libx::operator=(lib_libx const & rhs) {
+	if (this != &rhs)
+	{
+		this->mlx = rhs.mlx;
+		this->win = rhs.win;
+		this->img_new = rhs.img_new;
+		this->img_data = rhs.img_data;
+		this->winx = rhs.winx;
+		this->winy = rhs.winy;
+		this->red = rhs.red;
+		this->green = rhs.green;
+		this->blue = rhs.blue;
+		this->endian = rhs.endian;
+		this->line_size = rhs.line_size;
+		this->bit_pixel = rhs.bit_pixel;
+	}
+	return *this;
 }
 
-lib_libx::lib_libx( int x, int y, std::string *title ) :
-mlx(NULL), win(NULL), img_data(NULL), img_new(NULL), winx(800), winy(800),
+lib_libx::lib_libx( int x, int y, char *title ) :
+mlx(NULL), win(NULL), img_new(NULL), img_data(NULL), winx(x * 10), winy(y * 10),
 red(0), green(0), blue(0), endian(0), line_size(0), bit_pixel(0) {
-
-	static_cast<void *>(x); // use it after
-	static_cast<void *>(y); // use it after
 
 	this->mlx = mlx_init();
 	if (NULL == this->mlx)
-		throw std::exception;
+		throw std::exception();
 
 	this->win = mlx_new_window(this->mlx, this->winx, this->winy, title);
 	if (NULL == this->win)
-		throw std::exception;
+		throw std::exception();
 
 	this->img_new = mlx_new_image(this->mlx, this->winx, this->winy);
-	if (NULL -- this->this->img_new)
-		throw std::exception;
+	if (NULL == this->img_new)
+		throw std::exception();
 
-	this->img_data = mlx_get_data_addr(d->img_data, &(this->bit_pixel), &(this->line_size), &(this->endian));
+	this->img_data = mlx_get_data_addr(this->img_data, &(this->bit_pixel), &(this->line_size), &(this->endian));
 	if (NULL == this->img_data)
-		throw std::exception;
+		throw std::exception();
 }
 
 lib_libx::~lib_libx( void ) {
--- lib_libx/libx.class.hpp
+++ lib_libx/libx.class.hpp
@@ -26,22 +26,22 @@ typedef struct		s_key {
 
 class	lib_libx : public api {
 public:
-	void	*img_new;
 	void	*mlx;
 	void	*win;
+	void	*img_new;
 	char	*img_data;
 	int		winx;
 	int		winy;
 	int		red;
-	int		freen;
+	int		green;
 	int		blue;
 	int		endian;
 	int		line_size;
 	int		bit_pixel;
 
-	lib_libx( int x, int y, std::string *title );
-	lib_libx( lib_libx const &bsrc );
-	lib_libx & operation=( lib_libx const &brhs );
+	lib_libx( int x, int y, char *title );
+	lib_libx( lib_libx const & src );
+	lib_libx & operator=( lib_libx const & rhs );
 	~lib_libx( void );
 };
 
