6f77e11c1ec74c49102c88c480a127f9f53ae57e
Jean-Jacques MOIROUX <jmoiroux@student.42.fr>
2015-03-23 17:38:20 +0100

    ajout source minilibx macos

--- lib_libx/libx.class.cpp
+++ lib_libx/libx.class.cpp
@@ -12,18 +12,41 @@
 
 #include "libx.class.hpp"
 
-lib_libx( lib_libx const &bsrc ) {
+lib_libx::lib_libx( lib_libx const &bsrc ) {
 
 }
 
-lib_libx & operation=(lib_libx const &brhs) {
+lib_libx::lib_libx & operation=(lib_libx const &brhs) {
 
 }
 
-lib_libx( void ) {
+lib_libx::lib_libx( int x, int y, std::string *title ) :
+mlx(NULL), win(NULL), img_data(NULL), img_new(NULL), winx(800), winy(800),
+red(0), green(0), blue(0), endian(0), line_size(0), bit_pixel(0) {
 
+	static_cast<void *>(x); // use it after
+	static_cast<void *>(y); // use it after
+
+	this->mlx = mlx_init();
+	if (NULL == this->mlx)
+		throw std::exception;
+
+	this->win = mlx_new_window(this->mlx, this->winx, this->winy, title);
+	if (NULL == this->win)
+		throw std::exception;
+
+	this->img_new = mlx_new_image(this->mlx, this->winx, this->winy);
+	if (NULL -- this->this->img_new)
+		throw std::exception;
+
+	this->img_data = mlx_get_data_addr(d->img_data, &(this->bit_pixel), &(this->line_size), &(this->endian));
+	if (NULL == this->img_data)
+		throw std::exception;
 }
 
-~lib_libx() {
-
+lib_libx::~lib_libx( void ) {
+	std::free(this->img_data);
+	std::free(this->img_new);
+	std::free(this->win);
+	std::free(this->mlx);
 }
--- lib_libx/libx.class.hpp
+++ lib_libx/libx.class.hpp
@@ -1,21 +1,54 @@
-#include "../includes/api.class.hpp"
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   libx.class.hpp                                     :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: jmoiroux <jmoiroux@student.42.fr>          +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2015/03/23 16:27:26 by jmoiroux          #+#    #+#             //
+//   Updated: 2015/03/23 16:27:29 by jmoiroux         ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
 
-class api;
 
-typedef struct		s_key
-{
+#ifndef LIBX_CLASS_HPP
+# define LIBX_CLASS_HPP
+
+# include "../includes/api.class.hpp"
+# include "minilibx_macos/mlx.h"
+# include <stdexcept>
+# include <vector>
+# include <string>
+# include <iostream>
+# include <strings.h>
+
+
+class 				api;
+
+typedef struct		s_key {
 	int				key;
 	struct s_key	*next;
 }					t_key;
 
-class lib_libx : public api {
+class	lib_libx : public api {
 public:
-	lib_libx( void );
+	void	*img_new;
+	void	*mlx;
+	void	*win;
+	char	*img_data;
+	int		winx;
+	int		winy;
+	int		red;
+	int		freen;
+	int		blue;
+	int		endian;
+	int		line_size;
+	int		bit_pixel;
+
+	lib_libx( int x, int y, std::string *title );
 	lib_libx( lib_libx const &bsrc );
-	lib_libx & operation=(lib_libx const &brhs);
-	~lib_libx();
-
-private:
-	
-
+	lib_libx & operation=( lib_libx const &brhs );
+	~lib_libx( void );
 };
+
+#endif
