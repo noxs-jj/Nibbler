228a04d5a07ca4d9d510270442ebe98b9fd1eade
Vincent Jacquier <vin.jacquier@gmail.com>
2015-03-28 22:34:40 +0100

    modif script sh

--- lib_sfml/1.install_sdl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#brew update
-#brew install sfml
-export LD_LIBRARY_PATH=~/.brew/Cellar/sfml/2.2/lib
-echo "sfml done"
---
+++ set_shell.sh
@@ -0,0 +1,2 @@
+#!/bin/zsh
+export LD_LIBRARY_PATH=~/.brew/Cellar/sfml/2.2/lib
